da567fbb9af36c8c3e76d511ccb5cef2d1c8ba3a
laolsson <l.a.olsson@gmail.com>
2014-09-04 15:07:23 +1000

    link  to real content in summary

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,8 @@
 
+var cache = {}
+/* invalidate cache every 10 min */
+(function() { cache = {}; }).periodical(1000 * 60 * 10); //10 minutes
+
 function display_feeds(){
 	$.get('/json/feeds',function(r){
                render_feeds(r);
@@ -28,15 +32,13 @@ function display_feed(id){
 function render_feed(r){
 		var tmp = '<div id="feed">';
 		
-		/*$.each(r.feed,function(){*/
-			tmp = tmp + '<div class="row"><b>' + r.feed.title + '</b> Update: ' + r.feed.last_update + '<a href="/update_feed?id=' + r.feed.key + '">Update</a> <a href="/update_feed?id=' + r.feed.key + '">All</a><a href="/update_feed?id=' + r.feed.key + '">Unread</a></div>'
-		//});
+		tmp = tmp + '<div class="row"><b>' + r.feed.title + '</b> Update: ' + r.feed.last_update + '<a href="/update_feed?id=' + r.feed.key + '">Update</a> <a href="/update_feed?id=' + r.feed.key + '">All</a><a href="/update_feed?id=' + r.feed.key + '">Unread</a></div>'
 		
 		$.each(r.items,function(){
 			var title = this.title;
 			if(!this.read)
 				title = '<b>' + title + '</b>'
-			tmp = tmp + ('<div class="row"><a href="/read?id=' + this.id + ' " target="_blank">' + title + '</a> <a href="/read?id=' + this.id + ' " target="_blank"> -></a> ' + this.date + '</div>')
+			tmp = tmp + ('<div class="row"><a href="javascript:toggle(' + this.id + ')">'+ title + '</a>' + this.date + '</div><div id="' + this.id + '" style="display: none">' + this.summary + '<br><br><a href="/read?id=' + this.id + ' " target="_blank"> <- Read more -> </a><br><br></div>')
         });
     tmp = tmp + '</div>';            
 	$("#feed").replaceWith(tmp)
